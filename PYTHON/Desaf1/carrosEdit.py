c8ace8e431e65cfd215031f157e7a3f3b81c7be1
Oscar Daniel <oscardanieliafratelopez@gmail.com>
2024-09-04 21:45:35 -0400

    organized  y added desafpy2folder with a .py and .txt new file:   PYTHON/Desaf1/carrosEdit.py new file:   PYTHON/desafpy2/4.txt renamed:    PYTHON/Desaf1/carrosVer3.py -> PYTHON/desafpy2/5.carrosVer3.py

---
+++ PYTHON/Desaf1/carrosEdit.py
@@ -0,0 +1,48 @@
+# las weas que hacen las funciones funcionar jsjsjs
+from random import randint
+
+# entradas
+date = int (input ("que fecha es hoy (dd/mm/aaaa)"))
+car = input ("que modelo quieres alquilar?")
+time = int (input ("cuantos dias"))
+# magic
+dayCost = randint (25,50)
+
+# procesos
+totalCost = int (dayCost*time) 
+downPayment = totalCost*0.35 
+lastday = int (time+1)
+
+
+# salida
+if time>=10 :
+       print (f"""
+       **************************
+       Fecha {date}
+       el coste por dia es de: {dayCost}$
+       el costo total es: {totalCost}$
+       el pago inicial es 35% del total: {downPayment}$
+       recuerde devolver el modelo: 
+       "{car}" con el tanque lleno y como el tiempo de {time} dias supera los 10
+       debe pagar un monto adicional de deposito de 100$ además
+       con un max de {lastday} dia(s) para evitar costos extra
+       **************************
+       Gracias por alquilar el vehiculo en [REDACTADO]
+       conserva la factura hasta terminar el alquiler!
+       **************************
+       """)
+else :
+       print (f"""
+       **************************
+       Fecha {date}
+       el coste por dia es de: {dayCost}$
+       el costo total es: {totalCost}$
+       el pago inicial es 35% del total: {downPayment}$
+       recuerde devolver el modelo: 
+       "{car}" con el tanque lleno y 
+       con un max de {lastday} dia(s) para evitar costos extra
+       **************************
+       Gracias por alquilar el vehiculo en [REDACTADO]
+       conserva la factura hasta terminar el alquiler!
+       **************************
+       """)
