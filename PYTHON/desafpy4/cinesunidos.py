694bb11a405f1e06274eda0d8bdf781d00fa855e
Oscar Daniel <oscardanieliafratelopez@gmail.com>
2024-09-20 23:29:00 -0400

    desafio 4 de python2

---
+++ PYTHON/desafpy4/cinesunidos.py
@@ -0,0 +1,78 @@
+while True:
+    #edad
+    age = int(input("  la edad (solo numeros)\n  "))
+
+    #dia
+    print (f"""
+        cual dia de la semana (escribe cualquiera de estos)
+        lun - mar - mie - jue - vie
+        sab - dom
+        """)
+    
+    dia = input().lower()
+    match dia:
+        case "lun":
+            desclun = .5
+        case "mar":
+            desclun = 1
+        case "mie":
+            desclun = 1
+        case "jue":
+            desclun = 1
+        case "vie":
+            desclun = 1
+        case "sab":
+            desclun = 1
+        case "dom":
+            desclun = 1
+        case _:
+            desclun = 1
+            print ("debes escribir un dia, ej:-lun-")
+
+    #hora
+    print (f"""
+        cual hora del dia (escribe tal y como se muestran a la izquierda)
+        1pm = 1$ 
+        2pm = 2$
+        3pm = 3$
+        otras horas = 4$
+        """)
+    hora = input().lower()
+    match hora:
+        case "1pm":
+            price = 1
+        case "2pm":
+            price = 2
+        case "3pm":
+            price = 3
+        case "otras horas":
+            price = 4
+        case _:
+            price = 0
+            print("debes escribir 1, 2 o 3 junto con pm, o escribir ``otras horas``")
+
+    #process
+    if age<2 or age>70:
+        desclun = 0
+    elif age<10:
+        if desclun>0:
+            desclun = .5
+    
+    finalprice=price*desclun
+    
+    
+    resp= input("deseas comprar otro boleto?\n s/n\n ").lower()
+
+    #output
+    print(f"""
+        ===========Boleto==========
+        dia de la funcion: {dia}        
+        hora de la funcion: {hora}
+        monto de la entrada: {price}$
+        descuento : {desclun}
+        monto final: {finalprice}$
+        ===========================
+    """)
+    if resp !="s":
+        break
+
---
+++ PYTHON/desafpy4/olimpics.py
@@ -0,0 +1,7 @@
+nums = [2000,2004,2008,2012,2016,2020,2024]
+for n in nums:
+    print(f"en los years: {n} hubo juegos olimpicos (simple)")
+#con range:
+#range(min, max, step)
+for i in range(2000,2024,4):
+    print(f"en los years: {i} hubo juegos olimpicos (range)")
