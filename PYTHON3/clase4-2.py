5888831aec4dc1ed449dcb7594a24b0e68f77abf
OSC4R <oscardanieliafratelopez@gmail.com>
2024-10-19 14:36:53 -0400

    clase4pythonfinal

--- PYTHON3/clase4-2.py
+++ PYTHON3/clase4-2.py
@@ -2,7 +2,7 @@ def procesar():
     cont_nike=0 #contador especifico para la marca nike
     acum_nike=0 #acumulador especifico de los precios de los zapatos
     cont_adidas=0
-    acum=adidas=0
+    acum_adidas=0
     while True:
         marca=input("Ingrese la marca a procesar").lower()
         precio=float(input("Ingrese el precio de la marca"))
@@ -12,14 +12,13 @@ def procesar():
         if marca=="adidas":
             cont_adidas=cont_adidas+1
             acum_adidas=acum_adidas+precio
-        if marca=="":
-            b=0
         #controlar el ciclo
         resp=input("Presione espacio para dentener")
         if resp==" ":
             break
-    return cont_nike, acum_nike
+    return cont_nike, acum_nike,acum_adidas, acum_adidas
 
+#modulo para calcular promedios
 def promediar(acum, cont):
     if cont>0:
         promedio=acum/cont
@@ -27,18 +26,30 @@ def promediar(acum, cont):
         promedio=0
     return promedio
 
+#modulo para calcular porcentajes
+def porcentaje(total_zapatos, cont2):
+    porc=cont2/(cont1*100)
+    return porc
+
+#calculador de nike
 def calcular_n(cont_nike, acum_nike):
     prom_nike=promediar(acum_nike,cont_nike)
-    return prom_nike
-
+    porc_nike=porcentaje()
+    return prom_nike, porc_nike
+    
+#calculador de adidas
 def calcular_a(cont_adidas, acum_adidas):
     prom_adidas=promediar(acum_adidas,cont_adidas)
-    return prom_adidas
+    porc_adidas=porcentaje()
+    return prom_adidas, porc_adidas
     
-def imprimir(prom_nike):
+#calculador de zapatos
+def totalzapatos(cont_nike,cont_adidas):
+    cont_zapatos=cont_adidas+cont_zapatos
+
+#printer
+def imprimir(prom_nike,prom_adidas):
     print(f"el promedio de precios de zapatos nike es: {prom_nike}")
+    print(f"el promedio de precios de zapatos nike es: {prom_adidas}")
 
 #cuerpop principal
-c_n,a_n=procesar()
-p=calcular_n(c_n,a_n)
-print(p)
---
+++ PYTHON3/clase4-4.py
@@ -0,0 +1,6 @@
+print("ciclos dobles")
+
+for i in range(1,11):
+    print(f"tabla del {i}")
+    for j in range(11):
+        print(f"{i} * {j} = {i*j}")
