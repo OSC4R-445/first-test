02f917a71de50415d9d4a82bb35d748b10e12678
OSC4R <oscardanieliafratelopez@gmail.com>
2024-10-19 13:23:02 -0400

    Commit1

--- PYTHON3/clase4-2.py
+++ PYTHON3/clase4-2.py
@@ -1,12 +1,19 @@
 def procesar():
     cont_nike=0 #contador especifico para la marca nike
     acum_nike=0 #acumulador especifico de los precios de los zapatos
+    cont_adidas=0
+    acum=adidas=0
     while True:
         marca=input("Ingrese la marca a procesar").lower()
         precio=float(input("Ingrese el precio de la marca"))
         if marca=="nike":
             cont_nike=cont_nike+1
             acum_nike=acum_nike+precio
+        if marca=="adidas":
+            cont_adidas=cont_adidas+1
+            acum_adidas=acum_adidas+precio
+        if marca=="":
+            b=0
         #controlar el ciclo
         resp=input("Presione espacio para dentener")
         if resp==" ":
@@ -20,14 +27,18 @@ def promediar(acum, cont):
         promedio=0
     return promedio
 
-def calcular(cont_nike, acum_nike):
+def calcular_n(cont_nike, acum_nike):
     prom_nike=promediar(acum_nike,cont_nike)
     return prom_nike
+
+def calcular_a(cont_adidas, acum_adidas):
+    prom_adidas=promediar(acum_adidas,cont_adidas)
+    return prom_adidas
     
 def imprimir(prom_nike):
     print(f"el promedio de precios de zapatos nike es: {prom_nike}")
 
 #cuerpop principal
 c_n,a_n=procesar()
-p=calcular(c_n,a_n)
+p=calcular_n(c_n,a_n)
 print(p)
