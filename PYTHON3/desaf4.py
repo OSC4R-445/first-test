a04ee0f9dc3a0e8ce3c8ccd7abe98cf0d3fa1f8c
Oscar Daniel <oscardanieliafratelopez@gmail.com>
2024-10-23 22:57:30 -0400

    ono

--- PYTHON3/desaf4.py
+++ PYTHON3/desaf4.py
@@ -23,7 +23,7 @@ def calc(monto_total_factura, iva, monto_promedio):
 def calc_menos1000(monto_total_factura, i, monto_menosDe1000, porc_monto_menosDe1000):
     if monto_total_factura<1000:
         monto_menosDe1000=monto_menosDe1000+1
-        porc_monto_menosDe1000=monto_menosDe1000/i
+        porc_monto_menosDe1000=(monto_menosDe1000/i)
 
     return monto_menosDe1000, porc_monto_menosDe1000
 
@@ -39,10 +39,11 @@ def sucursales(monto_promedio_sucursales, monto_total_factura_sucursales):
     monto_total_factura=0
     monto_menosDe1000_sucursal=0
     monto_total_factura_sucursal=0
+    porc_monto_total_factura_sucursal=0
 
     print("#####################PROGRAMA_START#######################")
     #sucursales
-    for n in range(1,2):
+    for n in range(1,5):
         print(f"||||===================sucursal {n}===================||||")
         nombre_sucursal, ciudad_sucursal, cantEmpleados_sucursal=entrada_sucursal()
 
@@ -66,7 +67,7 @@ def sucursales(monto_promedio_sucursales, monto_total_factura_sucursales):
             print("factura: ", monto_total_factura)
             print("iva: ", iva)
             print("monto promedio: ", monto_promedio)
-            if monto_menosDe1000>1:
+            if monto_total_factura<1000:
                 print("esta factura tiene menos de 1000")
             print("=======================================")
             breaker=input("si quieres ya NO quieres hacer otra factura presiona espacio")
@@ -75,11 +76,12 @@ def sucursales(monto_promedio_sucursales, monto_total_factura_sucursales):
 
         monto_total_factura_sucursal=monto_total_factura_sucursal+monto_total_factura
         monto_promedio_sucursal=monto_promedio_sucursal+monto_promedio
-
-        monto_menosDe1000_sucursal=monto_menosDe1000_sucursal+monto_menosDe1000
         
+        monto_menosDe1000_sucursal=monto_menosDe1000_sucursal+monto_menosDe1000
+        porc_monto_total_factura_sucursal=(monto_menosDe1000_sucursal/4)
+
         print(f"MONTO TOTAL FACTURADO EN LA SUCURSAL {monto_total_factura_sucursal}")
-        print(f"porcentage de las facturas con un monto menor a 1000 EN LA SUCURSAL {porc_monto_menosDe1000}")
+        print(f"porcentage de las facturas con un monto menor a 1000 EN LA SUCURSAL: {porc_monto_total_factura_sucursal}")
         
         print("||||==================================================||||")
     monto_promedio_sucursales, monto_total_factura_sucursales=promedio_sucursales(monto_promedio_sucursal, monto_total_factura_sucursal, monto_promedio_sucursales, monto_total_factura_sucursales)
