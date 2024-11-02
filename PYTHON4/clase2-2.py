bb980a9a101f1f021af4c7762bea629cd0d9c94e
OSC4R <oscardanieliafratelopez@gmail.com>
2024-11-02 12:47:49 -0400

    Clase2 Python4-2

--- PYTHON4/clase2-2.py
+++ PYTHON4/clase2-2.py
@@ -1,3 +1,5 @@
+notas=[13,12,20,10,9,5,7]
+
 def acumular(lista):
     acumulador=0
     for i in range(len(lista)):
@@ -19,6 +21,22 @@ def procentaje(conte,contg):
     porc=conte/contg*100
     return porc
 
-
 def print_select(lista):
     print ("listado de notas inferioresa a 10 puntos")
+    for n in lista:
+        if n<10:
+            print(n)
+
+def load_dat(arreglo):
+    print("carga de datos")
+    for i in range(len(arreglo)):
+        print("ingrese el dato")
+        num=int(input("Ingrese el dato"))
+        arreglo[i]=num
+        
+
+#main body 
+
+acumular(notas)
+
+promediar()
