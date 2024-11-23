43b319dbaad7105372192ca209008cacdb570880
OSC4R <you@example.com>
2024-11-23 13:39:21 -0400

    Clase4-3+

--- PYTHON4/clase4-3.py
+++ PYTHON4/clase4-3.py
@@ -7,7 +7,6 @@ def cargar_string(msj, min, max):
         else:
             return dato
 
-
 def validar_positivo(msj):
     num=-1
     while not num>0:
@@ -18,29 +17,6 @@ def validar_positivo(msj):
         else:
             return num
 
-def cargar(arreglo1, arreglo2):
-    print("carga de datos")
-    while True:
-        nombre=cargar_string("Ingrese el nombre", 2, 12)
-        ci=validar_positivo("Ingrese la cedula")
-        existe=buscar(ci,arreglo2)
-        
-        if existe:
-            print("esta cedula ya esta registrada")
-            continue
-        else:
-            arreglo1.append(ci)
-            arreglo1.append(nombre)
-
-        resp=input("Presione espacio para detener la carga")
-        if resp== " ":
-            break
-
-def imprimir(arreglo, nombre_arreglo, arreglo2, nombre_arreglo2):
-    print(f"Datos a mostrar: {nombre_arreglo}, {nombre_arreglo2}")
-    for i in range(len(arreglo)):
-        print(arreglo[i], " -> ", arreglo2[i])
-
 def buscar(dato, arreglo):
     find=False
     i=0
@@ -49,6 +25,29 @@ def buscar(dato, arreglo):
             find=True
         i+1
     return find
+
+def cargar(arreglo1, arreglo2):
+    print("carga de datos")
+    while True:
+        nombre=cargar_string("Ingrese el nombre", 2, 12)
+        ci=validar_positivo("Ingrese la cedula")
+        existe=buscar(ci,arreglo2)
+
+        if existe:
+            print("esta cedula ya esta registrada")
+            continue
+        else:
+            arreglo1.append(nombre)
+            arreglo2.append(ci)
+
+        resp=input("Presione espacio para detener la carga")
+        if resp== " ":
+            break
+
+def imprimir(arreglo, nombre_arreglo, arreglo2, nombre_arreglo2):
+    print(f"Datos a mostrar: {nombre_arreglo}, {nombre_arreglo2}")
+    for i in range(len(arreglo)):
+        print(f"{arreglo[i]} -> {arreglo2[i]}")
 
 def validar_digitos(msj):
     while True:
